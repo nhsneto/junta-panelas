f79070bb8d348c1fd7eba478bebedd7a7efed3ba
Nelson Neto <nhsneto@gmail.com>
2024-06-03 00:56:51 -0300

    Add ViewsTest

---
+++ tests/Feature/ViewsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+test('home page can be rendered', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+});
+
+test('registration page can be rendered', function () {
+    $response = $this->get('/cadastro');
+
+    $response->assertStatus(200);
+});
