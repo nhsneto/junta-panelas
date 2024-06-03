a6cb2e0b50222d514b1054837fca67b28802d41f
Nelson Neto <nhsneto@gmail.com>
2024-06-03 18:59:32 -0300

    Add form error messages on the register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,10 +4,10 @@
 
         <form method="POST" action="/cadastro" class="w-full flex flex-col gap-y-6">
             @csrf
-            <x-form-field label="Nome" name="nome"/>
-            <x-form-field label="Email" type="email" name="email"/>
-            <x-form-field label="Senha" type="password" name="senha"/>
-            <x-form-field label="Confirmar Senha" type="password" name="senha_confirmation"/>
+            <x-form-field label="Nome" name="nome" :value="old('nome')" required/>
+            <x-form-field label="Email" type="email" name="email" :value="old('email')" required/>
+            <x-form-field label="Senha" type="password" name="senha" required/>
+            <x-form-field label="Confirmar Senha" type="password" name="senha_confirmation" required/>
             <button type="submit" class="bg-[#f0997d] text-xl text-black/50 font-bold text-center py-2.5 rounded-xl drop-shadow-md hover:brightness-110 active:brightness-100">Cadastrar</button>
         </form>
 
--- resources/views/components/form-field.blade.php
+++ resources/views/components/form-field.blade.php
@@ -1,6 +1,14 @@
-@props(['label', 'type' => 'text', 'name', 'id' => $name])
+@props([
+    'label',
+    'name',
+    'type' => 'text',
+    'id' => $name,
+    'value' => '',
+    'required' => false,
+])
 
 <div class="flex flex-col gap-y-2">
     <label for="{{ $id }}" class="font-semibold">{{ $label }}</label>
-    <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" class="px-3 py-2 border border-black/10 rounded" />
+    <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" value="{{ $value }}" {{ $required ? 'required' : '' }} class="px-3 py-2 border border-black/10 rounded"/>
+    <x-input-error :messages="$errors->get($name)"/>
 </div>
