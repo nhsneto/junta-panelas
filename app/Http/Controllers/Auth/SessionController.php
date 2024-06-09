2f5b82bd8f2501548660bf437ceb439b783bc341
Nelson Neto <nhsneto@gmail.com>
2024-06-09 02:11:49 -0300

    Remove unused imports

--- app/Http/Controllers/Auth/SessionController.php
+++ app/Http/Controllers/Auth/SessionController.php
@@ -5,9 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
 class SessionController extends Controller
