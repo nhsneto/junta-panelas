b2d88e2546f3620ffe2e6d1d3eef6a9a269a52dd
Nelson Neto <nhsneto@gmail.com>
2024-06-11 20:03:08 -0300

    Add placeholder attr to the form-field component

--- resources/views/components/form-field.blade.php
+++ resources/views/components/form-field.blade.php
@@ -3,12 +3,21 @@
     'name',
     'type' => 'text',
     'id' => $name,
+    'placeholder' => '',
     'value' => '',
     'required' => false,
 ])
 
 <div class="flex flex-col gap-y-2">
     <label for="{{ $id }}" class="font-semibold">{{ $label }}</label>
-    <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" value="{{ $value }}" {{ $required ? 'required' : '' }} class="px-3 py-2 border border-black/10 rounded"/>
+    <input
+        type="{{ $type }}"
+        name="{{ $name }}"
+        id="{{ $id }}"
+        placeholder="{{ $placeholder }}"
+        value="{{ $value }}"
+        {{ $required ? 'required' : '' }}
+        class="px-3 py-2 border border-black/10 rounded"
+    />
     <x-input-error :messages="$errors->get($name)"/>
 </div>
