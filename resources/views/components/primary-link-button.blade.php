34e0c4248362dcc68605377f79ca262c445f9467
Nelson Neto <nhsneto@gmail.com>
2024-06-11 00:24:51 -0300

    Change primary link button's default style

--- resources/views/components/primary-link-button.blade.php
+++ resources/views/components/primary-link-button.blade.php
@@ -1,7 +1,7 @@
 @props(['href' => '#'])
 
 @php
-    $classes = 'block bg-[#f0997d] text-xl text-black/50 font-bold text-center py-4 rounded-xl drop-shadow-md hover:brightness-110 active:brightness-100';
+    $classes = 'bg-[#f0997d] text-sm text-black/50 font-bold text-center px-4 py-2 rounded-md drop-shadow-md hover:brightness-110 active:brightness-100';
 @endphp
 
 <a href="{{ $href }}" {{ $attributes->merge(['class' => $classes]) }}>{{ $slot }}</a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
         <div class="max-w-md">
             <h1 class="text-5xl font-bold leading-tight">{{ __('Plan Your Junta-Panelas now!') }}</h1>
             <p class="mt-4 text-xl leading-snug">{{ __('When will be the junta-panelas? What will people should bring?') }}</p>
-            <x-primary-link-button href="{{ route('login') }}" class="mt-4">{{ __('Plan now!') }}</x-primary-link-button>
+            <x-primary-link-button href="{{ route('login') }}" class="mt-4 block text-xl py-4 rounded-xl">{{ __('Plan now!') }}</x-primary-link-button>
         </div>
         <img src="{{ url('storage/pexels-fauxels-3184195-640.jpg') }}" alt="{{ __('People having lunch at a table') }}" class="rounded-md drop-shadow-md">
     </section>
