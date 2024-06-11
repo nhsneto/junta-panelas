6e14c1b017c7570d7207a72ba0907ef64d06f806
Nelson Neto <nhsneto@gmail.com>
2024-06-11 00:41:14 -0300

    Add dashboard content

---
+++ resources/views/components/junta-panelas/card.blade.php
@@ -0,0 +1,8 @@
+<article {{ $attributes->merge(['class' => 'group bg-[#fbfbfb] px-5 py-4 rounded-xl shadow-md']) }}>
+    <div class="flex gap-x-2 items-center">
+        <h2 class="text-lg font-bold">Confraternização do Curso de Inglês</h2>
+        <a href="#" class="hidden group-hover:inline">Icon</a>
+    </div>
+    <p class="mt-1 text-sm text-black/50 font-bold">31/05/2024 · 11:30</p>
+    <x-primary-link class="mt-4 inline-block text-sm font-bold">{{ __('Participants') }}</x-primary-link>
+</article>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,8 @@
 <x-layout>
-    <section>{{ $user->name }}</section>
+    <div class="mt-6 w-2/3 flex flex-col">
+        <x-primary-link-button class="w-32 px-9 py-3 font-extrabold">{{ __('Plan') }}</x-primary-link-button>
+        <div class="mt-12 space-y-5">
+            <x-junta-panelas.card/>
+        </div>
+    </div>
 </x-layout>
