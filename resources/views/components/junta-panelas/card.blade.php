44ae4c7fc43155c82be81e72f418a35ab614fdd9
Nelson Neto <nhsneto@gmail.com>
2024-06-11 21:29:44 -0300

    Add icons to junta-panelas card

--- resources/views/components/junta-panelas/card.blade.php
+++ resources/views/components/junta-panelas/card.blade.php
@@ -1,8 +1,19 @@
 <article {{ $attributes->merge(['class' => 'group bg-[#fbfbfb] px-5 py-4 rounded-xl shadow-md']) }}>
     <div class="flex gap-x-2 items-center">
         <h2 class="text-lg font-bold">Confraternização do Curso de Inglês</h2>
-        <a href="#" class="hidden group-hover:inline">Icon</a>
+        <a href="#" class="hidden px-1 py-1 rounded-md group-hover:inline hover:bg-black/5 active:bg-black/10">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+            </svg>
+        </a>
     </div>
+
     <p class="mt-1 text-sm text-black/50 font-bold">31/05/2024 · 11:30</p>
-    <x-primary-link class="mt-4 inline-block text-sm font-bold">{{ __('Participants') }}</x-primary-link>
+
+    <div class="group/participants mt-4 inline-flex items-center gap-x-1.5">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 group-hover/participants:text-[#d3756b]">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+        </svg>
+        <x-primary-link class="inline-block text-sm font-bold">{{ __('Participants') }}</x-primary-link>
+    </div>
 </article>
