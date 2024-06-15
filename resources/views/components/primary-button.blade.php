3dd7a0a957bba85183c9b89aa8b6a2a84610eb8c
Nelson Neto <nhsneto@gmail.com>
2024-06-15 19:57:54 -0300

    Remove padding style from primary button to make it flexible for other contexts

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,5 +1,5 @@
 @php
-    $classes = 'bg-[#f0997d] text-black/50 font-bold text-center py-2.5 rounded-xl drop-shadow-md hover:brightness-110 active:brightness-100';
+    $classes = 'bg-[#f0997d] text-black/50 font-bold text-center rounded-xl drop-shadow-md hover:brightness-110 active:brightness-100';
 @endphp
 
 <button {{ $attributes->merge(['class' => $classes]) }}>{{ $slot }}</button>
