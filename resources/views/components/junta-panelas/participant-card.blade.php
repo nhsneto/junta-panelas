b4470114860d7226a2cb23f9ff84f8a250f74be1
Nelson Neto <nhsneto@gmail.com>
2024-06-17 00:04:02 -0300

    Add csrf token to participant card's form

--- resources/views/components/junta-panelas/participant-card.blade.php
+++ resources/views/components/junta-panelas/participant-card.blade.php
@@ -2,6 +2,7 @@
     <div class="flex justify-between gap-x-2">
         <p class="font-bold text-black/50">Nelson</p>
         <form method="POST" action="">
+            @csrf
             <button class="hidden px-1 py-1 rounded hover:bg-black/5 active:bg-black/10 hover:text-[#c82333] group-hover:flex">
                 <x-trash-icon />
             </button>
