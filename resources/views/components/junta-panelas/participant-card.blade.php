054b2bc475ac59e10e6e2916c0dfb3b13e546abc
Nelson Neto <nhsneto@gmail.com>
2024-06-16 23:04:52 -0300

    Create participant-card component

---
+++ resources/views/components/junta-panelas/participant-card.blade.php
@@ -0,0 +1,11 @@
+<div class="group flex flex-col basis-full gap-y-2 px-6 py-3 bg-[#fbfbfb] rounded-lg shadow-md">
+    <div class="flex justify-between gap-x-2">
+        <p class="font-bold text-black/50">Nelson</p>
+        <form method="POST" action="">
+            <button class="hidden px-1 py-1 rounded hover:bg-black/5 active:bg-black/10 hover:text-[#c82333] group-hover:flex">
+                <x-trash-icon />
+            </button>
+        </form>
+    </div>
+    <p class="text-sm font-semibold text-black/50">Brigadeiro · Coca-cola</p>
+</div>
