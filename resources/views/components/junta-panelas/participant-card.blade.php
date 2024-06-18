2e4bb217b82df62b48a0cac79b88329ea267ad31
Nelson Neto <nhsneto@gmail.com>
2024-06-17 21:18:18 -0300

    Change div to article in participant card

--- resources/views/components/junta-panelas/participant-card.blade.php
+++ resources/views/components/junta-panelas/participant-card.blade.php
@@ -1,6 +1,6 @@
-<div class="group flex flex-col basis-full gap-y-2 px-6 py-3 bg-[#fbfbfb] rounded-lg shadow-md">
+<article class="group flex flex-col basis-full gap-y-2 px-6 py-3 bg-[#fbfbfb] rounded-lg shadow-md">
     <div class="flex justify-between gap-x-2">
-        <p class="font-bold text-black/50">Nelson</p>
+        <h2 class="font-bold text-black/50">Nelson</h2>
         <form method="POST" action="">
             @csrf
             <button class="hidden px-1 py-1 rounded hover:bg-black/5 active:bg-black/10 hover:text-[#c82333] group-hover:flex">
@@ -9,4 +9,4 @@
         </form>
     </div>
     <p class="text-sm font-semibold text-black/50">Brigadeiro · Coca-cola</p>
-</div>
+</article>
