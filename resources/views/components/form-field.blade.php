7ca7a35c09b970e3937765b5554fb42409f941a9
Nelson Neto <nhsneto@gmail.com>
2024-06-20 22:59:17 -0300

    Make placeholder and value attributes optional

--- resources/views/components/form-field.blade.php
+++ resources/views/components/form-field.blade.php
@@ -15,10 +15,10 @@
         type="{{ $type }}"
         name="{{ $name }}"
         id="{{ $id }}"
-        placeholder="{{ $placeholder }}"
-        value="{{ $value }}"
-        {{ $min ? "min=$min" : '' }}
-        {{ $required ? 'required' : '' }}
+        @if($placeholder) placeholder="{{ $placeholder }}" @endif
+        @if($value) value="{{ $value }}" @endif
+        @if($min) min="{{ $min }}" @endif
+        @if($required) required @endif
         class="px-3 py-2 border border-black/10 rounded"
     />
     <x-input-error :messages="$errors->get($name)"/>
