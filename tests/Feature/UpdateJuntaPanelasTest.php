92b7a4eb78aaa78863f6ff8368aa9e9c39aaf46c
Nelson Neto <nhsneto@gmail.com>
2024-06-21 04:13:56 -0300

    Add validation tests in UpdateJuntaPanelasTest

--- tests/Feature/UpdateJuntaPanelasTest.php
+++ tests/Feature/UpdateJuntaPanelasTest.php
@@ -86,3 +86,177 @@ test('should do nothing when trying to update a junta-panelas planning with no n
 
     $response->assertRedirectToRoute('junta-panelas.edit');
 });
+
+test('should fail when trying to update a junta-panelas planning without title', function () {
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->post(route('login'), [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $this->post(route('junta-panelas.store'), [
+        'title' => 'Test title',
+        'date' => date('Y-m-d'),
+        'time' => '08:20',
+    ]);
+
+    $id = JuntaPanelas::first()->id;
+
+    $response = $this->put("junta-panelas/{$id}", [
+        'title' => null,
+        'date' => now()->modify('+ 1 day')->format('Y-m-d'),
+        'time' => '16:00',
+    ]);
+
+    $response->assertInvalid('title');
+});
+
+test('should fail when trying to update a junta-panelas planning whose title has more than 255 characters', function () {
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->post(route('login'), [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $this->post(route('junta-panelas.store'), [
+        'title' => 'Test title',
+        'date' => date('Y-m-d'),
+        'time' => '08:20',
+    ]);
+
+    $id = JuntaPanelas::first()->id;
+
+    $response = $this->put("junta-panelas/{$id}", [
+        'title' => 'Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test T',
+        'date' => now()->modify('+ 1 day')->format('Y-m-d'),
+        'time' => '16:00',
+    ]);
+
+    $response->assertInvalid('title');
+});
+
+test('should fail when trying to update a junta-panelas planning with no date', function () {
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->post(route('login'), [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $this->post(route('junta-panelas.store'), [
+        'title' => 'Test title',
+        'date' => date('Y-m-d'),
+        'time' => '08:20',
+    ]);
+
+    $id = JuntaPanelas::first()->id;
+
+    $response = $this->put("junta-panelas/{$id}", [
+        'title' => 'Test title',
+        'date' => null,
+        'time' => '16:00',
+    ]);
+
+    $response->assertInvalid('date');
+});
+
+test('should fail when trying to update a junta-panelas planning with a date that already passed', function () {
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->post(route('login'), [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $this->post(route('junta-panelas.store'), [
+        'title' => 'Test title',
+        'date' => date('Y-m-d'),
+        'time' => '08:20',
+    ]);
+
+    $id = JuntaPanelas::first()->id;
+
+    $response = $this->put("junta-panelas/{$id}", [
+        'title' => 'Test title',
+        'date' => now()->subDay()->format('Y-m-d'),
+        'time' => '16:00',
+    ]);
+
+    $response->assertInvalid('date');
+});
+
+test('should fail when trying to update a junta-panelas planning without time', function () {
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->post(route('login'), [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $this->post(route('junta-panelas.store'), [
+        'title' => 'Test title',
+        'date' => date('Y-m-d'),
+        'time' => '08:20',
+    ]);
+
+    $id = JuntaPanelas::first()->id;
+
+    $response = $this->put("junta-panelas/{$id}", [
+        'title' => 'Test title',
+        'date' => now()->modify('+ 1 day')->format('Y-m-d'),
+        'time' => null,
+    ]);
+
+    $response->assertInvalid('time');
+});
+
+test('should fail when trying to update a junta-panelas planning with time in wrong format', function () {
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->post(route('login'), [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ]);
+
+    $this->post(route('junta-panelas.store'), [
+        'title' => 'Test title',
+        'date' => date('Y-m-d'),
+        'time' => '08:20',
+    ]);
+
+    $id = JuntaPanelas::first()->id;
+
+    $response = $this->put("junta-panelas/{$id}", [
+        'title' => 'Test title',
+        'date' => now()->modify('+ 1 day')->format('Y-m-d'),
+        'time' => '06:15:20', // Only accepts time in HH:MM format
+    ]);
+
+    $response->assertInvalid('time');
+});
