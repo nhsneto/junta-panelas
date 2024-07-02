d7ea1f7b6614fa2aadc02c0e4bb5f7e589178dfc
Nelson Neto <nhsneto@gmail.com>
2024-07-02 19:41:47 -0300

    Add create junta-panelas with modal using ajax request (jquery)

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Junta-Panelas App</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Style+Script&display=swap" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
         @vite('resources/js/app.js')
     </head>
     <body class="h-full text-black/65">
--- resources/views/junta-panelas/index.blade.php
+++ resources/views/junta-panelas/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="mt-6 w-2/3 flex flex-col">
         <div id="stickyMenu" class="w-full mt-10 py-6 pl-8 sticky top-0 z-10 rounded-md bg-[#fbfbfb] drop-shadow">
-            <x-primary-link-button href="{{ route('junta-panelas.create') }}" class="inline-block px-9 py-3">{{ __('Plan') }}</x-primary-link-button>
+            <button id="openCreateModal" class="btn px-8 border-none bg-[#f0997d] hover:bg-[#ee8c6d]">{{ __('Plan') }}</button>
         </div>
 
         <div class="mt-5 overflow-x-auto rounded-md bg-[#fbfbfb] px-8 py-5 shadow">
@@ -33,5 +33,103 @@
                 @endforeach
             </table>
         </div>
+
+        <dialog id="createModal" class="modal bg-black/40">
+            <div class="modal-box px-10 bg-[#fbfbfb]">
+                <h1 class="text-2xl text-center font-bold">{{ __('Planning') }}</h1>
+
+                <form class="mt-10 flex flex-col gap-y-6">
+                    <div>
+                        <x-form-field label="{{ __('Event Title') }}" name="title" placeholder="{{ __('Christmas Party') }}" :value="old('title')"/>
+                        <ul id="titleErrors" class="mt-2 text-sm text-red-600 space-y-1"></ul>
+                    </div>
+
+                    <div>
+                        <x-form-field label="{{ __('Date') }}" type="date" name="date" :value="old('date')"/>
+                        <ul id="dateErrors" class="mt-2 text-sm text-red-600 space-y-1"></ul>
+                    </div>
+
+                    <div>
+                        <x-form-field label="{{ __('Time') }}" type="time" name="time" :value="old('time')"/>
+                        <ul id="timeErrors" class="mt-2 text-sm text-red-600 space-y-1"></ul>
+                    </div>
+                </form>
+
+                <div class="mt-10 modal-action space-x-5">
+                    <button id="closeCreateModal"  class="btn border-transparent rounded-md font-semibold hover:border-[#f0997d] shadow-none">{{ __('Cancel') }}</button>
+                    <button id="createButton" class="btn px-5 border-none bg-[#f0997d] hover:bg-[#ee8c6d]">{{ __('Plan') }}</button>
+                </div>
+            </div>
+        </dialog>
     </div>
+
+    <script>
+        window.onload = function () {
+            const createModal = document.getElementById("createModal");
+
+            $("#openCreateModal").on("click", () => {
+                createModal.show();
+            });
+
+            $("#closeCreateModal").on("click", () => {
+                createModal.close();
+            });
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $("#createButton").on("click", () => {
+                removeErrors();
+
+                $.post({
+                    url: "http://localhost:8000/junta-panelas",
+                    type: "post",
+                    data: {
+                        title: $("#title").val(),
+                        date: $("#date").val(),
+                        time: $("#time").val()
+                    },
+                    success: function () {
+                        createModal.close();
+                        location.reload();
+                    },
+                    error: function (err) {
+                        if (err.status === 422) {
+                            showErrors(err.responseJSON.errors);
+                        } else {
+                            console.log(err);
+                        }
+                    }
+                });
+            })
+
+            function showErrors(errors) {
+                titleErrors = addErrors(errors.title);
+                dateErrors = addErrors(errors.date);
+                timeErrors = addErrors(errors.time);
+
+                if (titleErrors) $("#titleErrors").html(titleErrors).removeClass("hidden");
+                if (dateErrors) $("#dateErrors").html(dateErrors).removeClass("hidden");
+                if (timeErrors) $("#timeErrors").html(timeErrors).removeClass("hidden");
+            }
+
+            function addErrors(fieldErrors) {
+                errors = "";
+
+                for (const error in fieldErrors) {
+                    errors += `<li>${error}</li>`;
+                }
+                return fieldErrors;
+            }
+
+            function removeErrors() {
+                $("#titleErrors").empty().addClass("hidden");
+                $("#dateErrors").empty().addClass("hidden");
+                $("#timeErrors").empty().addClass("hidden");
+            }
+        };
+    </script>
 </x-layout>
