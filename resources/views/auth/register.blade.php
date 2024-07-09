1354468f589145d7d2eb178ec9b66b7862dcede9
Nelson Neto <nhsneto@gmail.com>
2024-07-09 20:21:26 -0300

    Make register page responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,16 @@
 <x-layout>
-    <section class="max-w-lg flex flex-col basis-full items-center gap-y-8 px-12 py-10 bg-[#fbfbfb] rounded-xl shadow-md">
-        <h1 class="text-3xl font-semibold">{{ __('Register') }}</h1>
+    <section class="max-w-lg flex flex-col basis-full items-center gap-y-5 px-8 py-5 bg-[#fbfbfb] rounded-xl shadow-md md:gap-y-8 md:px-12 md:py-10">
+        <h1 class="text-xl font-semibold md:text-3xl">{{ __('Register') }}</h1>
 
-        <form method="POST" action="{{ route('register') }}" class="w-full flex flex-col gap-y-6">
+        <form method="POST" action="{{ route('register') }}" class="w-full flex flex-col gap-y-5 md:gap-y-6">
             @csrf
             <x-form-field label="{{ __('Name') }}" name="name" :value="old('name')" required/>
             <x-form-field label="Email" type="email" name="email" :value="old('email')" required/>
             <x-form-field label="{{ __('Password') }}" type="password" name="password" required/>
             <x-form-field label="{{ __('Confirm Password') }}" type="password" name="password_confirmation" required/>
-            <x-primary-button class="py-3">{{ __('Register Now') }}</x-primary-button>
+            <x-primary-button class="py-3 text-sm md:text-base">{{ __('Register Now') }}</x-primary-button>
         </form>
 
-        <x-primary-link href="{{ route('login') }}" class="text-sm">{{ __('Already registered?') }}</x-primary-link>
+        <x-primary-link href="{{ route('login') }}" class="text-xs md:text-sm">{{ __('Already registered?') }}</x-primary-link>
     </section>
 </x-layout>
