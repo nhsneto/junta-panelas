0904ef771b37ebc6fce4bd972d9c8bd5ef12be76
Nelson Neto <nhsneto@gmail.com>
2024-07-09 18:19:40 -0300

    Make navbar responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,26 @@
 <nav class="flex justify-between items-center py-10 font-semibold">
-    <a href="{{ url('/') }}" class="font-['Style_Script'] text-5xl">Junta-Panelas</a>
-    <div class="flex items-center space-x-6">
-        @guest
+    <a href="{{ url('/') }}" class="font-['Style_Script'] text-3xl sm:text-5xl">Junta-Panelas</a>
+
+    @guest
+        <div class="dropdown dropdown-bottom dropdown-end sm:hidden">
+            <button>
+                <x-icons.bars class="size-7" />
+            </button>
+            <div tabindex="0" id="dropdownMenu" class="w-48 px-0 py-1.5 dropdown-content menu bg-[#fbfbfb] rounded z-[1] shadow">
+                <a href="{{ route('login') }}" class="px-4 py-2 hover:bg-black/5">{{ __('Log In') }}</a>
+                <a href="{{ route('register') }}" class="px-4 py-2 hover:bg-black/5">{{ __('Register') }}</a>
+            </div>
+        </div>
+
+        <div class="hidden items-center space-x-6 sm:flex">
             <x-primary-link href="{{ route('login') }}">{{ __('Log In') }}</x-primary-link>
             <x-primary-link href="{{ route('register') }}">{{ __('Register') }}</x-primary-link>
-        @endguest
+        </div>
+    @endguest
 
-        @auth
-            <x-primary-link href="{{ route('junta-panelas.index') }}">{{ __('My Junta-Panelas') }}</x-primary-link>
+    @auth
+        <div class="flex items-center space-x-6">
+            <x-primary-link href="{{ route('junta-panelas.index') }}" class="hidden sm:inline">{{ __('My Junta-Panelas') }}</x-primary-link>
 
             <div class="flex dropdown dropdown-bottom dropdown-end">
                 <button>
@@ -22,6 +35,6 @@
                     </form>
                 </div>
             </div>
-        @endauth
-    </div>
+        </div>
+    @endauth
 </nav>
