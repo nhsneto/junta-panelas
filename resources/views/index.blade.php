048c082b21d28f9908d9c89ff626bcbf79135ba3
Nelson Neto <nhsneto@gmail.com>
2024-07-09 19:44:26 -0300

    Make home page responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 <x-layout>
-    <section class="w-full flex justify-between items-center">
+    <section class="w-full flex flex-col justify-center items-center gap-y-10 sm:flex-row sm:gap-y-0 sm:gap-x-10 lg:justify-between">
+        <img src="{{ url('storage/pexels-fauxels-3184195-640.jpg') }}" alt="{{ __('People having lunch at a table') }}" class="w-full max-w-[385px] rounded-md drop-shadow-md sm:min-w-[385px] sm:max-w-[480px] lg:min-w-[480px] lg:max-w-[640px]">
         <div class="max-w-md">
-            <h1 class="text-5xl font-bold leading-tight">{{ __('Plan Your Junta-Panelas now!') }}</h1>
-            <p class="mt-4 text-xl leading-snug">{{ __('When will be the junta-panelas? What will people should bring?') }}</p>
-            <x-primary-link-button href="{{ route('login') }}" class="mt-4 block text-xl py-4 rounded-xl">{{ __('Plan now!') }}</x-primary-link-button>
+            <h1 class="text-xl font-bold leading-tight md:text-2xl md:leading-tight lg:text-5xl lg:leading-tight">{{ __('Plan Your Junta-Panelas now!') }}</h1>
+            <p class="mt-4 text-sm leading-snug md:text-base md:leading-snug lg:text-xl lg:leading-snug">{{ __('When will be the junta-panelas? What will people should bring?') }}</p>
+            <x-primary-link-button href="{{ route('login') }}" class="mt-4 block text-lg py-4 rounded-xl lg:text-xl">{{ __('Plan now!') }}</x-primary-link-button>
         </div>
-        <img src="{{ url('storage/pexels-fauxels-3184195-640.jpg') }}" alt="{{ __('People having lunch at a table') }}" class="rounded-md drop-shadow-md">
     </section>
 </x-layout>
