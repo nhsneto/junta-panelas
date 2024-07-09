c3f038176392295adc93d29d50163fb20e867813
Nelson Neto <nhsneto@gmail.com>
2024-07-09 17:15:22 -0300

    Create navbar component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,33 +13,7 @@
     </head>
     <body class="h-full text-black/65">
         <div class="max-w-screen-xl h-full m-auto flex flex-col px-5">
-            <nav class="flex justify-between items-center py-10 font-semibold">
-                <a href="{{ url('/') }}" class="font-['Style_Script'] text-5xl">Junta-Panelas</a>
-                <div class="flex items-center space-x-6">
-                    @guest
-                        <x-primary-link href="{{ route('login') }}">{{ __('Log In') }}</x-primary-link>
-                        <x-primary-link href="{{ route('register') }}">{{ __('Register') }}</x-primary-link>
-                    @endguest
-
-                    @auth
-                        <x-primary-link href="{{ route('junta-panelas.index') }}">{{ __('My Junta-Panelas') }}</x-primary-link>
-
-                        <div class="flex dropdown dropdown-bottom dropdown-end">
-                            <button id="dropdownTrigger">
-                                <x-icons.user-circle class="size-9" />
-                            </button>
-                            <div tabindex="0" id="dropdownMenu" class="w-48 px-0 py-1.5 dropdown-content menu bg-[#fbfbfb] rounded z-[1] shadow">
-                                <a href="{{ route('profile') }}" class="px-4 py-2 hover:bg-black/5">{{ __('Profile') }}</a>
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button class="w-full text-left px-4 py-2 hover:bg-black/5">{{ __('Log Out') }}</button>
-                                </form>
-                            </div>
-                        </div>
-                    @endauth
-                </div>
-            </nav>
+            <x-navbar />
 
             <main class="@guest mt-auto @endguest flex justify-center">{{ $slot }}</main>
 
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="flex justify-between items-center py-10 font-semibold">
+    <a href="{{ url('/') }}" class="font-['Style_Script'] text-5xl">Junta-Panelas</a>
+    <div class="flex items-center space-x-6">
+        @guest
+            <x-primary-link href="{{ route('login') }}">{{ __('Log In') }}</x-primary-link>
+            <x-primary-link href="{{ route('register') }}">{{ __('Register') }}</x-primary-link>
+        @endguest
+
+        @auth
+            <x-primary-link href="{{ route('junta-panelas.index') }}">{{ __('My Junta-Panelas') }}</x-primary-link>
+
+            <div class="flex dropdown dropdown-bottom dropdown-end">
+                <button id="dropdownTrigger">
+                    <x-icons.user-circle class="size-9" />
+                </button>
+                <div tabindex="0" id="dropdownMenu" class="w-48 px-0 py-1.5 dropdown-content menu bg-[#fbfbfb] rounded z-[1] shadow">
+                    <a href="{{ route('profile') }}" class="px-4 py-2 hover:bg-black/5">{{ __('Profile') }}</a>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @method('DELETE')
+                        @csrf
+                        <button class="w-full text-left px-4 py-2 hover:bg-black/5">{{ __('Log Out') }}</button>
+                    </form>
+                </div>
+            </div>
+        @endauth
+    </div>
+</nav>
