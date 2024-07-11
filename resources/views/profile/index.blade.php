e41f9baccfa169909e53683f4405f275c6d75968
Nelson Neto <nhsneto@gmail.com>
2024-07-11 20:28:32 -0300

    Make profile page responsive

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,30 +1,30 @@
 <x-layout>
     <div class="max-w-2xl flex flex-col basis-full items-center gap-y-20">
-        <section class="w-full flex flex-col items-center gap-y-8 px-12 py-10 bg-[#fbfbfb] rounded-xl shadow-md">
-            <h2 class="text-xl font-semibold self-start">{{ __('Change Email') }}</h2>
+        <section class="w-full flex flex-col items-center gap-y-5 px-8 py-5 bg-[#fbfbfb] rounded-xl shadow-md md:gap-y-8 md:px-12 md:py-10">
+            <h2 class="font-bold self-start md:text-xl">{{ __('Change Email') }}</h2>
             <form method="POST" action="{{ route('email.update') }}" class="w-full flex flex-col gap-y-6 text-sm">
                 @method('put')
                 @csrf
                 <x-form-field label="{{ __('Current Email') }}" type="email" name="current_email" :value="old('current_email')" required/>
                 <x-form-field label="{{ __('New Email') }}" type="email" name="new_email" :value="old('new_email')" required/>
                 <x-form-field label="{{ __('Confirm New Email') }}" type="email" name="new_email_confirmation" required/>
-                <x-primary-button class="self-start px-5 py-2.5">{{ __('Change Email') }}</x-primary-button>
+                <x-primary-button class="self-start px-5 py-2.5 text-sm md:text-base">{{ __('Change Email') }}</x-primary-button>
             </form>
         </section>
 
-        <section class="w-full flex flex-col basis-full items-center gap-y-8 px-12 py-10 bg-[#fbfbfb] rounded-xl shadow-md">
-            <h2 class="text-xl font-semibold self-start">{{ __('Change Password') }}</h2>
+        <section class="w-full flex flex-col items-center gap-y-5 px-8 py-5 bg-[#fbfbfb] rounded-xl shadow-md md:gap-y-8 md:px-12 md:py-10">
+            <h2 class="font-bold self-start md:text-xl">{{ __('Change Password') }}</h2>
             <form method="POST" action="{{ route('password.update') }}" class="w-full flex flex-col gap-y-6 text-sm">
                 @method('put')
                 @csrf
                 <x-form-field label="{{ __('Current Password') }}" type="password" name="current_password" required/>
                 <x-form-field label="{{ __('New Password') }}" type="password" name="new_password" required/>
                 <x-form-field label="{{ __('Confirm New Password') }}" type="password" name="new_password_confirmation" required/>
-                <x-primary-button class="self-start px-5 py-2.5">{{ __('Change Password') }}</x-primary-button>
+                <x-primary-button class="self-start px-5 py-2.5 text-sm md:text-base">{{ __('Change Password') }}</x-primary-button>
             </form>
         </section>
 
-        <section class="w-full px-12 py-4 bg-[#fbfbfb] rounded-xl shadow-md">
+        <section class="w-full px-6 py-4 bg-[#fbfbfb] rounded-xl shadow-md md:px-12">
             <button id="openDeleteModal" class="flex items-center gap-x-1.5 px-2 py-1 text-sm text-[#c82333] rounded hover:bg-black/5">
                 <x-icons.trash />
                 <span>{{ __('Delete Account') }}</span>
