aff78711aba84e705f1c8be9761de1b9c5eb3215
Nelson Neto <nhsneto@gmail.com>
2024-07-14 01:17:32 -0300

    Create public img folder

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <section class="w-full flex flex-col justify-center items-center gap-y-10 sm:flex-row sm:gap-y-0 sm:gap-x-10 lg:justify-between">
-        <img src="{{ url('storage/pexels-fauxels-3184195-640.jpg') }}" alt="{{ __('People having lunch at a table') }}" class="w-full max-w-[385px] rounded-md drop-shadow-md sm:min-w-[385px] sm:max-w-[480px] lg:min-w-[480px] lg:max-w-[640px]">
+        <img src="{{ url('img/pexels-fauxels-3184195-640.jpg') }}" alt="{{ __('People having lunch at a table') }}" class="w-full max-w-[385px] rounded-md drop-shadow-md sm:min-w-[385px] sm:max-w-[480px] lg:min-w-[480px] lg:max-w-[640px]">
         <div class="max-w-md">
             <h1 class="text-xl font-bold leading-tight md:text-2xl md:leading-tight lg:text-5xl lg:leading-tight">{{ __('Plan Your Junta-Panelas now!') }}</h1>
             <p class="mt-4 text-sm leading-snug md:text-base md:leading-snug lg:text-xl lg:leading-snug">{{ __('When will be the junta-panelas? What will people should bring?') }}</p>
